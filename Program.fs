1621e83c8adfa5d61fbe6599e2ff0f5e2b229c31
Severin Sverdvik <severisv@gmail.com>
2016-07-30 18:28:42 +0200

    mapRow

--- Program.fs
+++ Program.fs
@@ -11,26 +11,29 @@ type Team = {
 }
 
 type Game = {
-    Date : string
+    Date : DateTime
     Division : string
     HomeTeam : Team
     AwayTeam : Team
   }
 
+
 type GamesFile = CsvProvider<"./data/15.csv">
 
-let gameList = GamesFile.GetSample()
+let mapRows (rows:seq<GamesFile.Row>) = rows |> Seq.map ( fun c -> { Division = c.Div; Date = DateTime.Parse c.Date; HomeTeam = { Name = c.HomeTeam; Score = c.FTHG; Shots = c.HS; ShotsOnTarget = c.HST; Corners = c.HC; Fouls = c.HF }; AwayTeam = { Name = c.AwayTeam; Score = c.FTAG; Shots = c.AS; ShotsOnTarget = c.AST; Corners = c.AC; Fouls = c.AF }} ) 
 
-let getGames = gameList.Rows 
-            |> Seq.map ( fun c -> { Division = c.Div; Date = c.Date; HomeTeam = { Name = c.HomeTeam; Score = c.FTHG; Shots = c.HS; ShotsOnTarget = c.HST; Corners = c.HC; Fouls = c.HF }; AwayTeam = { Name = c.AwayTeam; Score = c.FTAG; Shots = c.AS; ShotsOnTarget = c.AST; Corners = c.AC; Fouls = c.AF }} ) 
-    
 
+
+let games15 = GamesFile.Load("../../data/15.csv").Rows |> mapRows
+let games16 = GamesFile.Load("../../data/16.csv").Rows |> mapRows
+ 
+ 
 [<EntryPoint>]
 let main argv =
-    
-    let games = getGames
-    let count = Seq.length games
-
-    printf "%i" count
+    let games = Seq.append games15 games16
+   
+    for game in games |> Seq.take 5 do
+        printf "%s \n" (game.Date.ToShortDateString())
+        
     let s = Console.ReadLine()
     0 // return an integer exit code
