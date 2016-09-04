c34f1e378f409c18ee67d90e90565e265f9c2249
Severin Sverdvik <severisv@gmail.com>
2016-09-04 16:23:10 +0200

    Use CsvLoader

--- GameRepository.fs
+++ GameRepository.fs
@@ -3,28 +3,15 @@
 open Models
 open Game
 open FSharp.Data
+open FSharp.Data.CsvExtensions
 open System
 
-type GamesFile14 = CsvProvider<"./data/sample_14.csv">
-type GamesFile15 = CsvProvider<"./data/sample_15.csv">
-type GamesFile1617 = CsvProvider<"./data/sample_1617.csv">
 
-let mapRows14 (rows:seq<GamesFile14.Row>) = rows |> Seq.map ( fun c -> { Division = c.Div; Date = DateTime.Parse c.Date; 
-        HomeTeam = { Name = c.HomeTeam; Score = c.FTHG; Shots = c.HS; ShotsOnTarget = c.HST; Corners = c.HC; Fouls = c.HF }; 
-        AwayTeam = { Name = c.AwayTeam; Score = c.FTAG; Shots = c.AS; ShotsOnTarget = c.AST; Corners = c.AC; Fouls = c.AF };
-        Odds = { H = float c.B365H; U = float c.B365D;  B = float c.B365A } } ) 
+let mapRows (rows:seq<CsvRow>) = rows |> Seq.map ( fun c -> { Division = c?Div; Date = DateTime.Parse c?Date; 
+        HomeTeam = { Name = c?HomeTeam; Score = c?FTHG.AsInteger(); Shots = c?HS.AsInteger(); ShotsOnTarget = c?HST.AsInteger(); Corners = c?HC.AsInteger(); Fouls = c?HF.AsInteger() }; 
+        AwayTeam = { Name = c?AwayTeam; Score = c?FTAG.AsInteger(); Shots = c?AS.AsInteger(); ShotsOnTarget = c?AST.AsInteger(); Corners = c?AC.AsInteger(); Fouls = c?AF.AsInteger() };
+        Odds = { H = float c?B365H; U = float c?B365D;  B = float c?B365A } } )
 
-
-let mapRows15 (rows:seq<GamesFile15.Row>) = rows |> Seq.map ( fun c -> { Division = c.Div; Date = DateTime.Parse c.Date; 
-        HomeTeam = { Name = c.HomeTeam; Score = c.FTHG; Shots = c.HS; ShotsOnTarget = c.HST; Corners = c.HC; Fouls = c.HF }; 
-        AwayTeam = { Name = c.AwayTeam; Score = c.FTAG; Shots = c.AS; ShotsOnTarget = c.AST; Corners = c.AC; Fouls = c.AF };
-        Odds = { H = float c.B365H; U = float c.B365D;  B = float c.B365A } } ) 
-
-
-let mapRows1617 (rows:seq<GamesFile1617.Row>) = rows |> Seq.map ( fun c -> { Division = c.Div; Date = DateTime.Parse c.Date; 
-        HomeTeam = { Name = c.HomeTeam; Score = c.FTHG; Shots = c.HS; ShotsOnTarget = c.HST; Corners = c.HC; Fouls = c.HF }; 
-        AwayTeam = { Name = c.AwayTeam; Score = c.FTAG; Shots = c.AS; ShotsOnTarget = c.AST; Corners = c.AC; Fouls = c.AF };
-        Odds = { H = float c.B365H; U = float c.B365D;  B = float c.B365A } } ) 
 
 
 let getResource season = 
@@ -39,9 +26,7 @@ let getResource season =
 
 let loadGames season = 
     let resource = getResource season
-    if season.Year = 14 then GamesFile14.Load(resource).Rows |> mapRows14
-    else if season.Year = 15 then GamesFile15.Load(resource).Rows |> mapRows15
-    else GamesFile1617.Load(resource).Rows |> mapRows1617
+    CsvFile.Load(resource).Cache().Rows |> mapRows
     
 
 
